7bac17e4fb5c6b5a03e2bec7e93ab145c19dd9e6
Richard Dimalanta <chardmd.au@gmail.com>
2018-04-02 12:56:13 +1000

    Fixed a href links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -16,19 +16,19 @@ function Footer(props) {
             <div className="col-lg-6 h-100 text-center text-lg-left my-auto">
               <ul className="list-inline mb-2">
                 <li className="list-inline-item">
-                  <a href="#">About</a>
+                  <a href="/">About</a>
                 </li>
                 <li className="list-inline-item">&sdot;</li>
                 <li className="list-inline-item">
-                  <a href="#">Contact</a>
+                  <a href="/">Contact</a>
                 </li>
                 <li className="list-inline-item">&sdot;</li>
                 <li className="list-inline-item">
-                  <a href="#">Terms of Use</a>
+                  <a href="/">Terms of Use</a>
                 </li>
                 <li className="list-inline-item">&sdot;</li>
                 <li className="list-inline-item">
-                  <a href="#">Privacy Policy</a>
+                  <a href="/">Privacy Policy</a>
                 </li>
               </ul>
               <p className="text-muted small mb-4 mb-lg-0">
@@ -38,17 +38,17 @@ function Footer(props) {
             <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
               <ul className="list-inline mb-0">
                 <li className="list-inline-item mr-3">
-                  <a href="#">
+                  <a href="/">
                     <i className="fa fa-facebook fa-2x fa-fw" />
                   </a>
                 </li>
                 <li className="list-inline-item mr-3">
-                  <a href="#">
+                  <a href="/">
                     <i className="fa fa-twitter fa-2x fa-fw" />
                   </a>
                 </li>
                 <li className="list-inline-item">
-                  <a href="#">
+                  <a href="/">
                     <i className="fa fa-instagram fa-2x fa-fw" />
                   </a>
                 </li>
